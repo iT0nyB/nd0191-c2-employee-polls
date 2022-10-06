bc2fec2e1362a3e40ddc8cd357e672755ff65954
Anthony Baptiste <anthony.baptiste@outlook.com>
2022-10-06 14:32:15 -0400

    loaded initial data

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
-function App() {
+import { useEffect } from "react";
+import { connect } from "react-redux";
+import { handleInitialData } from "../actions/shared";
+
+const App = (props) => {
+  useEffect(() => {
+    props.dispatch(handleInitialData());
+  }, [props]);
   return (
     <div>
       <h3>Starting point</h3>
     </div>
   );
-}
+};
 
-export default App;
+export default connect()(App);
--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
+import { createStore } from "redux";
+import reducer from "./reducers";
+import middleware from "./middleware";
+import { Provider } from "react-redux";
+
+const store = createStore(reducer, middleware);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
