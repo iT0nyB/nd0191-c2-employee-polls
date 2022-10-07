9d9ed541b8e206fb5a903c9bb9ee06d35a68e84f
Anthony Baptiste <anthony.baptiste@outlook.com>
2022-10-07 00:08:20 -0400

    dashboard now splits questions into answered and new

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,21 +2,34 @@ import { connect } from "react-redux";
 import QuestionsContainer from "./QuestionsContainer";
 
 const Dashboard = (props) => {
-  const questionIds = props.questionIds;
+  const newQuestions = props.newQuestions;
+  const doneQuestions = props.doneQuestions;
+
   return (
     <div className="row">
       <div className="w-75 mx-auto">
-        <QuestionsContainer heading="New Questions" questionIds={questionIds} />
-        <QuestionsContainer heading="Done" questionIds={[]} />
+        <QuestionsContainer
+          heading="New Questions"
+          questionIds={newQuestions}
+        />
+        <QuestionsContainer heading="Done" questionIds={doneQuestions} />
       </div>
     </div>
   );
 };
 
-const mapStateToProps = ({ questions }) => ({
-  questionIds: Object.keys(questions).sort(
-    (a, b) => questions[b].timestamp - questions[a].timestamp
-  ),
-});
+const mapStateToProps = ({ questions, authedUser, users }) => {
+  const user = users[authedUser];
+  return {
+    questions: Object.values(questions),
+    doneQuestions: Object.keys(questions)
+      .filter((question) => Object.keys(user.answers).includes(question))
+      .sort((a, b) => questions[b].timestamp - questions[a].timestamp),
+    newQuestions: Object.keys(questions)
+      .filter((question) => !Object.keys(user.answers).includes(question))
+      .sort((a, b) => questions[b].timestamp - questions[a].timestamp),
+    user,
+  };
+};
 
 export default connect(mapStateToProps)(Dashboard);
